568d3147c2520b00ac202b405de23066dcdf84d2
Mohammad Momin <mominmohammad111@gmail.com>
2023-07-06 03:36:57 +0530

    Replaced baseUrl

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://api.weatherapi.com/v1/forecast.json?key=2bb31567bbba4101a31125526230407";
+const baseUrl = "https://api.weatherapi.com/v1/forecast.json?key=2bb31567bbba4101a31125526230407";
 
 export default baseUrl;
